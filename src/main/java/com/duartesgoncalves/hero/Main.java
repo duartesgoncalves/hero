06f61ed9c8b16a2fb5a7d3091b36d24cbe7f358c
Duarte Gonçalves <up202108772@edu.fe.up.pt>
2022-10-03 20:40:50 +0100

    Initialized a Lanterna Terminal and a Screen

--- src/main/java/com/duartesgoncalves/hero/Main.java
+++ src/main/java/com/duartesgoncalves/hero/Main.java
@@ -1,7 +1,13 @@
 package com.duartesgoncalves.hero;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args) {}
 }
